d26e6d28fbe66034cc72a9df5148fb636e0a4410
Viktr211 <cb777899@gmail.com>
2026-04-30 14:19:02 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,45 @@
+# config.py - API ключи для бирж
+# Заполните свои ключи между кавычками
+
+API_KEYS = {
+    "binance": {
+        "api_key": "",
+        "secret": ""
+    },
+    "kucoin": {
+        "api_key": "",
+        "secret": ""
+    },
+    "bybit": {
+        "api_key": "",
+        "secret": ""
+    },
+    "okx": {
+        "api_key": "",
+        "secret": ""
+    },
+    "gateio": {
+        "api_key": "",
+        "secret": ""
+    },
+    "bitget": {
+        "api_key": "",
+        "secret": ""
+    },
+    "bingx": {
+        "api_key": "",
+        "secret": ""
+    },
+    "mexc": {
+        "api_key": "",
+        "secret": ""
+    },
+    "hitbtc": {
+        "api_key": "",
+        "secret": ""
+    },
+    "poloniex": {
+        "api_key": "",
+        "secret": ""
+    }
+}
